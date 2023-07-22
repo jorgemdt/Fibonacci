b63b7e5fd79c286d03ea753aa778e283714fafd7
jorge <jorgerenato14@gmail.com>
2023-07-22 03:11:06 -0300

    implement numeric format strings

--- Program.cs
+++ Program.cs
@@ -12,20 +12,20 @@ namespace Fibonacci
                 Console.WriteLine("Digite quantos números quer na Sequência?( ATÉ 90.) ");
                 string input = Console.ReadLine();
 
-                if ( !long.TryParse(input, out long number) || number <= 0 || number > 90 )
+                if ( !ulong.TryParse(input, out ulong number) || number <= 0 || number > 90 )
                 {
                     Console.WriteLine("Apenas números inteiros entre 1 e 90!");
                     Console.WriteLine();
                     continue;
                 }
                                
-                long x = 1, y = 0, z = 0;
+                ulong x = 1, y = 0, z;
 
-                for ( long i = 0; i < number; i++ )
+                for ( ulong i = 0; i < number; i++ )
                 {
                     z = x + y;
 
-                    Console.WriteLine(z);
+                    Console.WriteLine(z.ToString("#,#,#,#,#,#"));
 
                     x = y; y = z;
                 }
