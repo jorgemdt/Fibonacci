fbec93ca5c9ff27e217157c73fcc35857d642165
jorge <jorgerenato14@gmail.com>
2023-07-23 00:25:38 -0300

    ui cganges

--- Program.cs
+++ Program.cs
@@ -8,17 +8,20 @@ namespace Fibonacci
         static void Main(string [ ] args)
         {
             while ( true )
-            {      
+            {
                 Console.WriteLine("Digite quantos números quer na Sequência?( ATÉ 90.) ");
                 string input = Console.ReadLine();
+                Console.WriteLine("====================================================");
 
                 if ( !ulong.TryParse(input, out ulong number) || number <= 0 || number > 90 )
                 {
                     Console.WriteLine("Apenas números inteiros entre 1 e 90!");
                     Console.WriteLine();
+                    Console.WriteLine("====================================================");
+
                     continue;
                 }
-                               
+
                 ulong x = 1, y = 0, z;
 
                 for ( ulong i = 0; i < number; i++ )
@@ -31,16 +34,18 @@ namespace Fibonacci
                 }
                 while ( true )
                 {
-                Console.WriteLine();
-                Console.WriteLine("Gostaria de continuar?(y/n)");
-                string resposta = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Gostaria de continuar?(y/n)");
+                    string resposta = Console.ReadLine();
+                    Console.WriteLine("====================================================");
+
 
                     if ( resposta == "y" )
                     {
                         break;
                     }
                     else if ( resposta == "n" )
-                    { 
+                    {
                         Environment.Exit(0);
                     }
                     else
