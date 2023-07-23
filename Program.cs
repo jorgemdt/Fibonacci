2198bf73e995b691a88de688623f2078d9d044dc
jorge <jorgerenato14@gmail.com>
2023-07-23 07:32:29 -0300

    increase the iteration limit

--- Program.cs
+++ Program.cs
@@ -9,28 +9,35 @@ namespace Fibonacci
         {
             while ( true )
             {
-                Console.WriteLine("Digite quantos números quer na Sequência?( ATÉ 90.) ");
+                Console.WriteLine("Digite quantos números quer na Sequência?( Maximo 93.) ");
                 string input = Console.ReadLine();
                 Console.WriteLine("====================================================");
 
-                if ( !ulong.TryParse(input, out ulong number) || number <= 0 || number > 90 )
+                if ( !ulong.TryParse(input, out ulong number) || number <= 0 || number > 93 )
                 {
-                    Console.WriteLine("Apenas números inteiros entre 1 e 90!");
+                    Console.WriteLine("Apenas números inteiros entre 1 e 93!");
                     Console.WriteLine();
                     Console.WriteLine("====================================================");
 
                     continue;
                 }
 
+                //ulong numberFinal = 12200160415121876738;
                 ulong x = 1, y = 0, z;
 
                 for ( ulong i = 0; i < number; i++ )
                 {
                     z = x + y;
-
-                    Console.WriteLine(z.ToString("#,#,#,#,#,#"));
-
+                    if ( y > z )
+                    {
+                        Console.WriteLine("impreciso...");
+                    }
+                    else
+                    {
+                        Console.WriteLine(z.ToString("#,#,#,#,#,#"));
+                    }
                     x = y; y = z;
+
                 }
                 while ( true )
                 {
